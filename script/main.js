b7589657da4325fa8b4378690bdcde147245bae2
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-04-05 13:16:48 +0200

    Creacion de repositorio

---
+++ script/main.js
@@ -0,0 +1,31 @@
+function calculadora(primerNumero, segundoNumero, operacion) {
+    switch (operacion) {
+        case "+":
+            return primerNumero + segundoNumero;
+        case "-":
+            return primerNumero - segundoNumero;
+        case "*":
+            return primerNumero * segundoNumero;
+        case "/":
+            return primerNumero / segundoNumero;
+        default:
+            return 0;
+    }
+}
+
+/*for (let i = 1; i <= 10; i++){
+    console.log("Eres el usuario Nro :" + " " + i)
+    if (i === 10){
+        alert("Eres nuestro cliente Nro 10, felicidades has ganado un 10% de descuento en tu compra!")
+        break
+    }
+}
+
+let precioProducto = Number(prompt("Ingrese el valor total de su prducto o productos (IVA del 21% se aplica despues junto con su 10% de descuento!)"))
+
+if (precioProducto == 0){
+    alert("Ingrese un monto")
+}else{
+    alert("Su total es:" + " " + "$" + ((precioProducto + (calculadora (precioProducto,0.21,"*"))) - (calculadora(precioProducto,0.10,"*")) ) )
+
+}*/
