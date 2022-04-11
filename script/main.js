1b563558755f744111c35d95db415269a73fd5e2
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-04-11 12:27:06 +0200

    adición de nombre en alert de nro usuario y emprolijamiento de codigo

--- script/main.js
+++ script/main.js
@@ -32,28 +32,37 @@ class Usuario{
     }
 }
 
-//Iteraciones
-for (let i = 1; i <= 10; i++){
-    console.log("Eres el usuario Nro :" + espacio + i)
-    if (i === 10){
-        alert("Eres nuestro cliente Nro 10, felicidades has ganado un 10% de descuento en tu compra!")
-        break
-    }
-}
-
 //Objeto1
 const usuario1 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su edad"), prompt("Ingrese su dirección"));
 usuario1.consoleInfoPersonal();
 
+//Iteraciones
+for (let i = 1; i <= 10; i++){
+    
+    if (i === 10){
+        alert("Felicidades"+ espacio + usuario1.nombre + espacio +"eres nuestro cliente Nro 10, ¡has ganado un 10% de descuento en tu compra!")
+        break
+    }
+}
+
 //Funcion Precio
-let precioProducto = prompt("Ingrese el valor total de su prducto o productos (IVA del 21% se aplica despues junto con su 10% de descuento!)")
+let precioProducto = prompt("Ingrese el valor total de su prducto o productos (¡IVA del 21% se aplica despues junto con su 10% de descuento!)")
+
 let precioInt = parseInt(precioProducto)
+
 if (precioProducto == ""){
+
     alert("Ingrese un monto")
+
 }else if (precioInt <=0) { 
+
     alert("ingrese un numero positivo")
+
 }else{
+
     let precioFinal = precioInt - (calculadora (precioInt,0.10,"*")) + (calculadora(precioInt,0.21,"*"))
+
     alert("Su total es :" + espacio + "$" + precioFinal )
+
     console.log("Este es el precio final :" + espacio + "$" + precioFinal)
 }
