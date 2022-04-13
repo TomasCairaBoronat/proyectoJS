4ab6ee2f60de510f4baec5b53437d490053c2c25
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-04-13 15:50:06 +0200

    correccion de calculadora con objetos y de console log de valores de objetos en arrays

--- script/main.js
+++ script/main.js
@@ -52,32 +52,40 @@ for (let i = 1; i <= 10; i++){
 
 //Comics
 
-const comic01 = new Comics(prompt("Ingrese el titulo del comic"), prompt("Ingrese el precio del comic"))
-const comic02 = new Comics(prompt("Ingrese el titulo del comic"), prompt("Ingrese el precio del comic"))
+const comic01 = new Comics(prompt("Ingrese el titulo del comic #1"), prompt("Ingrese el precio del comic #1"))
+const comic02 = new Comics(prompt("Ingrese el titulo del comic #2"), prompt("Ingrese el precio del comic #2"))
 
 //Arrays
 const comics = []
 comics.push(comic01);
 comics.push(comic02);
 
-console.log(comics);
+for (const comic of comics) {
+    console.log(comic.titulo + ":" + espacio +"$"+ comic.precio);
+}
 
-let precioNeto = comic01.precio + comic02.precio;
+let precioNeto = (comic01.precio + comic02.precio)
+
+let comicPrecio01int = parseInt(comic01.precio)
+
+let comicPrecio02int = parseInt(comic02.precio)
+
+
 //Funcion Precio
-let netoInt = parseInt(precioNeto)
+let netoInt = comicPrecio01int + comicPrecio02int;
 if (precioNeto == ""){
     
     alert("Ingrese un monto")
-
+    
 }else if (netoInt <=0) { 
-
+    
     alert("ingrese un numero positivo")
-
+    
 }else{
-
+    
     let precioFinal = netoInt - (calculadora (netoInt,0.10,"*")) + (calculadora(netoInt,0.21,"*"))
-
+    
     alert("Su total es :" + espacio + "$" + precioFinal )
-
-    console.log("Este es el precio final :" + espacio + "$" + precioFinal)
+    
+    console.log("Precio total :" + espacio + "$" + precioFinal)
 }
