e16c76102d05024c6e29c7b039255a744f80fd8a
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-04-15 11:07:31 +0200

    Emprolijamiento de codio JS

--- script/main.js
+++ script/main.js
@@ -1,39 +1,61 @@
 //Calculadora
 function calculadora(primerNumero, segundoNumero, operacion) {
     switch (operacion) {
+
         case "+":
+
             return primerNumero + segundoNumero;
+
         case "-":
+
             return primerNumero - segundoNumero;
+
         case "*":
+
             return primerNumero * segundoNumero;
+
         case "/":
+
             return primerNumero / segundoNumero;
+
         default:
+
             return 0;
+
     }
 }
 
 //Declaracion de shortcuts
-let espacio = " "
+let espacio = " ";
 
 //Clases
 class Usuario{
+
     constructor(nombre,edad,mail) {
+
         this.nombre = nombre.toUpperCase();
+
         this.edad = edad;
+
         this.mail = mail.toUpperCase();
+
     }
 
     alertInfoPersonal(){
-        alert("Nombre:" + this.nombre + espacio + "Edad:" + this.edad + espacio + "Dirección:" + this.direccion)
+
+        alert("Nombre:" + this.nombre + espacio + "Edad:" + this.edad + espacio + "Dirección:" + this.direccion);
+
     }
 }
 
 class Comics{
+
     constructor(titulo,precio) {
+
         this.titulo = titulo.toUpperCase();
+
         this.precio = precio;
+
     }
 }
 
@@ -49,24 +71,35 @@ const usuario4 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su eda
 
 const usuario5 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su edad"), prompt("Ingrese su mail"));
 
-const usuarios = [usuario1,usuario2,usuario3,usuario4]
-usuarios.push(usuario5)
+const usuarios = [usuario1,usuario2,usuario3,usuario4];
+
+usuarios.push(usuario5);
 
 for(const usuario of usuarios){
     while (usuario.nombre == ""){
-        alert("Ingrese su nombre correctamente")
-       usuario.nombre = prompt("Ingrese su nombre")
+
+        alert("Ingrese su nombre correctamente");
+
+       usuario.nombre = prompt("Ingrese su nombre");
+
     }
     while(usuario.edad == ""){
-        alert("Ingrese su edad correctamente")
-        usuario.edad = prompt("Ingrese su edad")
+
+        alert("Ingrese su edad correctamente");
+
+        usuario.edad = prompt("Ingrese su edad");
+
     }
 
     while(usuario.mail == ""){
-        alert("Ingrese su mail correctamente")
-        usuario.mail = prompt("Ingrese su mail")
+
+        alert("Ingrese su mail correctamente");
+
+        usuario.mail = prompt("Ingrese su mail");
+
     }
-    console.log(usuario)
+
+    console.log(usuario);
 }
 
 
@@ -75,46 +108,53 @@ for(const usuario of usuarios){
 for (let i = 1; i <= 5; i++){
     
     if (i === 5){
-        alert("Felicidades"+ espacio + usuario5.nombre + espacio +"eres nuestro cliente Nro 5, ¡has ganado un 10% de descuento en tu compra!")
+
+        alert("Felicidades"+ espacio + usuario5.nombre + espacio +"eres nuestro cliente Nro 5, ¡has ganado un 10% de descuento en tu compra!");
+
     }
 }
 
 //Comics
 
-const comic01 = new Comics(prompt("Ingrese el titulo del comic #1"), prompt("Ingrese el precio del comic #1"))
-const comic02 = new Comics(prompt("Ingrese el titulo del comic #2"), prompt("Ingrese el precio del comic #2"))
+const comic01 = new Comics(prompt("Ingrese el titulo del comic #1"), prompt("Ingrese el precio del comic #1"));
+
+const comic02 = new Comics(prompt("Ingrese el titulo del comic #2"), prompt("Ingrese el precio del comic #2"));
 
 //Arrays
 
-const comics = []
+const comics = [];
+
 comics.push(comic01,comic02);
 
 for (const comic of comics) {
+
     console.log(comic.titulo + ":" + espacio +"$"+ comic.precio);
+
 }
 
-let precioNeto = (comic01.precio + comic02.precio)
+let precioNeto = (comic01.precio + comic02.precio);
 
-let comicPrecio01int = parseInt(comic01.precio)
+let comicPrecio01int = parseInt(comic01.precio);
 
-let comicPrecio02int = parseInt(comic02.precio)
+let comicPrecio02int = parseInt(comic02.precio);
 
 
 //Funcion Precio
 let netoInt = comicPrecio01int + comicPrecio02int;
+
 if (precioNeto == ""){
     
-    alert("Ingrese un monto")
+    alert("Ingrese un monto");
     
 }else if (netoInt <=0) { 
     
-    alert("ingrese un numero positivo")
+    alert("ingrese un numero positivo");
     
 }else{
     
-    let precioFinal = netoInt - (calculadora (netoInt,0.10,"*")) + (calculadora(netoInt,0.21,"*"))
+    let precioFinal = netoInt - (calculadora (netoInt,0.10,"*")) + (calculadora(netoInt,0.21,"*"));
     
-    alert("Su total es:" + espacio + "$" + precioFinal )
+    alert("Su total es:" + espacio + "$" + precioFinal );
     
-    console.log("Precio total:" + espacio + "$" + precioFinal)
+    console.log("Precio total:" + espacio + "$" + precioFinal);
 }
