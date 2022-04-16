98b173eeb14a64a932a6d3bcdefd9a9b0a98ff54
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-04-16 15:51:02 +0200

    adicion de funciones y arrays para simplificar codigo

--- script/main.js
+++ script/main.js
@@ -28,115 +28,165 @@ function calculadora(primerNumero, segundoNumero, operacion) {
 //Declaracion de shortcuts
 let espacio = " ";
 
-//Clases
-class Usuario{
-
-    constructor(nombre,edad,mail) {
-
-        this.nombre = nombre.toUpperCase();
-
-        this.edad = edad;
-
-        this.mail = mail.toUpperCase();
-
-    }
-
-    alertInfoPersonal(){
-
-        alert("Nombre:" + this.nombre + espacio + "Edad:" + this.edad + espacio + "Dirección:" + this.direccion);
-
-    }
-}
-
-class Comics{
-
-    constructor(titulo,precio) {
-
-        this.titulo = titulo.toUpperCase();
-
-        this.precio = precio;
-
-    }
-}
-
 //Usuarios
 
-const usuario1 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su edad"), prompt("Ingrese su mail"));
+// Chequeo mail
+const mailRegEx = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-const usuario2 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su edad"), prompt("Ingrese su mail"));
+// Array de datos a ingresar:
+const datos = ['nombre', 'edad', 'mail']
 
-const usuario3 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su edad"), prompt("Ingrese su mail"));
+// Array donde ir guardando los usuarios:
+const usuarios = [];
 
-const usuario4 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su edad"), prompt("Ingrese su mail"));
+// Indice del array de usuarios:
+let index = 0;
 
-const usuario5 = new Usuario(prompt("Ingrese su nombre"), prompt("Ingrese su edad"), prompt("Ingrese su mail"));
-
-const usuarios = [usuario1,usuario2,usuario3,usuario4];
-
-usuarios.push(usuario5);
-
-for(const usuario of usuarios){
-    while (usuario.nombre == ""){
-
-        alert("Ingrese su nombre correctamente");
-
-       usuario.nombre = prompt("Ingrese su nombre");
-
-    }
-    while(usuario.edad == ""){
-
-        alert("Ingrese su edad correctamente");
-
-        usuario.edad = prompt("Ingrese su edad");
-
-    }
-
-    while(usuario.mail == ""){
-
-        alert("Ingrese su mail correctamente");
-
-        usuario.mail = prompt("Ingrese su mail");
-
-    }
-
-    console.log(usuario);
+// Clase Usuario:
+class Usuario {
+  
+  constructor (nombre, edad, mail) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.mail = mail;
+  }
+  
+  mostrarDataUsuario() {
+    alert(
+      'Nombre: ' + this.nombre + '\n' + 
+      'Edad: ' + this.edad + '\n' + 
+      'Mail: ' + this.mail
+    )
+  }
 }
 
+// Metodos de validacion de datos ingresados.
+const validacion = {
 
+  nombre: (input) => input.length < 3,
 
-//Iteraciones
-for (let i = 1; i <= 5; i++){
-    
-    if (i === 5){
+  edad: (input) => isNaN(input) || input > 100,
 
-        alert("Felicidades"+ espacio + usuario5.nombre + espacio +"eres nuestro cliente Nro 5, ¡has ganado un 10% de descuento en tu compra!");
-
-    }
+  mail: (input) => !input.match(mailRegEx)
 }
+
+// Funcion para ingresar datos de usuario:
+function ingresarDataUsuario(dato) {
+
+  let input;
+  
+  do {
+    input = prompt("Ingrese su " + dato)
+
+  } while (validacion[dato](input));
+  
+  return input
+}
+
+// Funcion para crear un nuevo usuario:
+function crearUsuario() {
+
+  usuarios[index] = new Usuario();
+
+  for (let dato of datos) {
+    usuarios[index][dato] = ingresarDataUsuario(dato)
+  }
+
+  usuarios[index].mostrarDataUsuario()
+
+  index++
+}
+
+do { 
+  crearUsuario()
+
+} while (index <= 4){
+
+    alert("Felicidades"+ espacio + usuarios[4].nombre + espacio +"eres nuestro cliente Nro 5, ¡has ganado un 10% de descuento en tu compra!");
+}
+console.log(usuarios)
+
 
 //Comics
 
-const comic01 = new Comics(prompt("Ingrese el titulo del comic #1"), prompt("Ingrese el precio del comic #1"));
 
-const comic02 = new Comics(prompt("Ingrese el titulo del comic #2"), prompt("Ingrese el precio del comic #2"));
+// Array de datos comics:
+const datosComic = ['titulo', 'precio']
 
-//Arrays
-
+// Array donde ir guardando los comics:
 const comics = [];
 
-comics.push(comic01,comic02);
+// Indice del array de comics:
+let indexComic = 0;
 
-for (const comic of comics) {
+// Clase Comic:
+class Comic {
+  
+  constructor (titulo,precio) {
+    this.titulo = titulo;
+    this.precio = precio;
 
-    console.log(comic.titulo + ":" + espacio +"$"+ comic.precio);
+  }
+  
+  mostrarDataComic() {
+    alert(
+      'Titulo: ' + this.titulo + '\n' + 
+      'Precio: ' + "$"+ this.precio
+    )
+  }
+}
+
+// Metodos de validacion de datos ingresados.
+const validacionComics = {
+
+  titulo: (input) => input.length < 3,
+
+  precio: (input) => isNaN(input) || input <= 0,
 
 }
 
-let precioNeto = (comic01.precio + comic02.precio);
+// Funcion para ingresar datos de comic:
+function ingresarDataComic(datosComic) {
 
-let comicPrecio01int = parseInt(comic01.precio);
+  let input;
+  
+  do {
+    input = prompt("Ingrese su " + datosComic)
 
-let comicPrecio02int = parseInt(comic02.precio);
+  } while (validacionComics[datosComic](input));
+  
+  return input.toUpperCase()
+}
+
+// Funcion para crear un nuevo comic:
+function crearComic() {
+
+  comics[indexComic] = new Comic();
+
+  for (let datoComic of datosComic) {
+    comics[indexComic][datoComic] = ingresarDataComic(datoComic)
+  }
+
+  comics[indexComic].mostrarDataComic()
+
+  indexComic++
+}
+
+do { 
+  crearComic()
+
+} while (indexComic <= 1);
+
+for (let comic of comics){
+    console.log('Titulo: ' + comic.titulo.toUpperCase() + '\n' + 
+    'Precio: ' + "$"+ comic.precio)
+}
+
+let precioNeto = (comics[0].precio + comics[1].precio);
+
+let comicPrecio01int = parseInt(comics[0].precio);
+
+let comicPrecio02int = parseInt(comics[1].precio);
 
 
 //Funcion Precio
@@ -152,7 +202,8 @@ if (precioNeto == ""){
     
 }else{
     
-    let precioFinal = netoInt - (calculadora (netoInt,0.10,"*")) + (calculadora(netoInt,0.21,"*"));
+    let precioConDescuento = netoInt - (calculadora (netoInt,0.10,"*"));
+    let precioFinal = precioConDescuento + (calculadora(precioConDescuento,0.21,"*"))
     
     alert("Su total es:" + espacio + "$" + precioFinal );
     
